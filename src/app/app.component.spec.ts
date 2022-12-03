881240d85e742b8370a116ff1690df08e55fbdaa
Janusz Kina <janusz@gmail.com>
2022-12-03 12:57:56 +0100

    updated gitignore, code cleaned

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  // it(`should have as title 'file-picker'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('file-picker');
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain(
-  //     'file-picker app is running!'
-  //   );
-  // });
 });
